c3b65bfc33c59135292234319c1ec1d02d7ad3b4
unknown <guilhermeboaventurarodrigues@gmail.com>
2022-07-06 18:33:17 -0300

    fixing indentation

--- deploy.go
+++ deploy.go
@@ -3,19 +3,26 @@ package main
 import (
 	"fmt"
 	"log"
-	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"github.com/ethereum/go-ethereum/ethclient"
-
 	"github.com/ethereum/go-ethereum/crypto"
 	"math/big"
 	"crypto/ecdsa"
 	"context"
+	token "github.com/guilhermeboaventurarodrigues/token-go/tokens"
 )
 
 func main() {
 
-privateKey, err := crypto.HexToECDSA("170c05471c13cde237a066e52deb893999d566dd548182da2f06770ee872e2b5")
+client, err := ethclient.Dial("HTTP://127.0.0.1:7545") //IP GANACHE
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("we have a connection")
+	_ = client
+
+	privateKey, err := crypto.HexToECDSA("170c05471c13cde237a066e52deb893999d566dd548182da2f06770ee872e2b5") // PRIVATE KEY
 
 	publicKey := privateKey.Public()
 	publicKeyECDSA, ok := publicKey.(*ecdsa.PublicKey)
@@ -40,13 +47,13 @@ privateKey, err := crypto.HexToECDSA("170c05471c13cde237a066e52deb893999d566dd54
 	auth.GasLimit = uint64(6721975)
 	auth.GasPrice = gasPrice
 
-	address, tx, instance, err := token.DeployToken(auth, client)
+	address, tx, instance, err := token.DeployToken(auth, client) 
 	if err != nil{
 		log.Fatal(err)
 	}
 
-	fmt.Println(address.Hex())
-	fmt.Println(tx.Hash().Hex())
+	fmt.Println(address.Hex()) // Address of contract
+	fmt.Println(tx.Hash().Hex()) //Address of transaction
 
 	_ = instance
 
