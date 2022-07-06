a7bfeba78c28a9311ff1bba65dc9544ccea01def
unknown <guilhermeboaventurarodrigues@gmail.com>
2022-07-06 18:25:09 -0300

    add deploy.go

---
+++ deploy.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+	"github.com/ethereum/go-ethereum/ethclient"
+
+	"github.com/ethereum/go-ethereum/crypto"
+	"math/big"
+	"crypto/ecdsa"
+	"context"
+)
+
+func main() {
+
+privateKey, err := crypto.HexToECDSA("170c05471c13cde237a066e52deb893999d566dd548182da2f06770ee872e2b5")
+
+	publicKey := privateKey.Public()
+	publicKeyECDSA, ok := publicKey.(*ecdsa.PublicKey)
+	if !ok {
+		log.Fatal("error casting public key to EDCSA")
+	}
+
+	fromAddress := crypto.PubkeyToAddress(*publicKeyECDSA)
+	nonce, err := client.PendingNonceAt(context.Background(), fromAddress)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	gasPrice, err := client.SuggestGasPrice(context.Background())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	auth := bind.NewKeyedTransactor(privateKey)
+	auth.Nonce = big.NewInt(int64(nonce))
+	auth.Value = big.NewInt(0)
+	auth.GasLimit = uint64(6721975)
+	auth.GasPrice = gasPrice
+
+	address, tx, instance, err := token.DeployToken(auth, client)
+	if err != nil{
+		log.Fatal(err)
+	}
+
+	fmt.Println(address.Hex())
+	fmt.Println(tx.Hash().Hex())
+
+	_ = instance
+
+}
